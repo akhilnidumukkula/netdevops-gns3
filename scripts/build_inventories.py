8e98279f5041269bfb9f99f9e5a29007c1da7dc7
anidumuk <akhilnidumukkula@gmail.com>
2020-11-21 16:10:29 -0500

    Update build_inventories.py
    updated inventories

--- scripts/build_inventories.py
+++ scripts/build_inventories.py
@@ -2,8 +2,8 @@ from app.lab import Lab
 
 def main():
     lab = Lab.create()
-    #lab.build_ansible_inventories("ansible/inventory")
-    #lab.build_ansible_inventories("ansible/inventory-big", random_data=True)
+    lab.build_ansible_inventories("ansible/inventory")
+    lab.build_ansible_inventories("ansible/inventory-big", random_data=True)
     lab.build_nornir_inventory("nornir/inventory")
     
 if __name__ == "__main__":
